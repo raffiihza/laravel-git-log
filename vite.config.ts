c1385cd1a7fbfc1c9dd8f011681dcdee3ab84aea
Raffi Ihza Zuhairnawan <115229257+raffiihza@users.noreply.github.com>
2025-09-30 01:53:04 +0000

    wrong again

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,7 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/gitstatus/',
-    build: {
-        assetsDir: 'assets',
-    },
+    base: '/gitstatus/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
